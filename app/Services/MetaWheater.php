36028773eafbfe48e3958ef00b070c6b2d38fe6e
Agung Yudha Berliantara <agung.berliantara@hostinger.com>
2020-07-29 09:56:09 +0700

    Fix for phpcs

--- app/Services/MetaWheater.php
+++ app/Services/MetaWheater.php
@@ -15,8 +15,8 @@ class MetaWheater
      * 
      */
     public static function getWeather($city){
-        $client = new Client;
-        $result = $client->request('GET', config('constants.META_WEATHER.QUERY_URL').$city);
+        $client = new Client();
+        $result = $client->request('GET', config('constants.META_WEATHER.QUERY_URL') . $city);
         $body = $result->getBody()->getContents();
         $body = json_decode($body, TRUE);
         if (empty($body)){
@@ -36,24 +36,24 @@ class MetaWheater
      * @return array a set array of result and assets for weather icon
      */
     private static function week($city, $woeid){
-        $client = new Client;
-        $result = $client->request('GET', config('constants.META_WEATHER.WOEID_URL').$woeid);
+        $client = new Client();
+        $result = $client->request('GET', config('constants.META_WEATHER.WOEID_URL') . $woeid);
         $result = $result->getBody()->getContents();
         $array_result = json_decode($result, TRUE);
         $weather_a_week = $array_result['consolidated_weather'];
         
         // data rendering
         $data = [];
-        foreach ($weather_a_week AS $key=>$value){
+        foreach ($weather_a_week AS $key => $value){
             $data[$key]['city'] = $city;
-            $data[$key]['temperature'] = round($value['the_temp'])."&deg;C";
+            $data[$key]['temperature'] = round($value['the_temp']) . "&deg;C";
             $data[$key]['days'] = $value['applicable_date'];
-            $data[$key]['max_temp'] = round($value['max_temp'])."&deg;C";
-            $data[$key]['min_temp'] = round($value['min_temp'])."&deg;C";
+            $data[$key]['max_temp'] = round($value['max_temp']) . "&deg;C";
+            $data[$key]['min_temp'] = round($value['min_temp']) . "&deg;C";
             $data[$key]['weather_name'] = $value['weather_state_name'];
-            $data[$key]['weather_icon'] = config('constants.META_WEATHER.IMG_URL').$value['weather_state_abbr'].'.svg';
-            $data[$key]['wind'] = round($value['wind_speed']*1.60934, 2).' km/h';
-            $data[$key]['direction'] = config('constants.COMPASS.'.$value['wind_direction_compass']);
+            $data[$key]['weather_icon'] = config('constants.META_WEATHER.IMG_URL') . $value['weather_state_abbr'] . '.svg';
+            $data[$key]['wind'] = round($value['wind_speed'] * 1.60934, 2) . ' km/h';
+            $data[$key]['direction'] = config('constants.COMPASS.' . $value['wind_direction_compass']);
         }
         return $data;
     }
@@ -83,17 +83,17 @@ class MetaWheater
                 if(array_key_exists($city, $array_city)){
                     $append[$city] = [
                         'status' => $array_city[$city]['status'],
-                        'total_hits' => $array_city[$city]['total_hits']+1
+                        'total_hits' => $array_city[$city]['total_hits'] + 1
                     ];
                     
                     unset($array_city[$city]);
-                    $array_city +=$append;
+                    $array_city += $append;
                 }else{
                     $append[$city] = [
                         'status' => $status,
                         'total_hits' => 1
                     ];
-                    $array_city +=$append;
+                    $array_city += $append;
                 }
                 // the file will be saved into storage/app/public directory
                 Storage::disk('public')->put('city.json', json_encode($array_city));
